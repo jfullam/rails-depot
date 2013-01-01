affcc7ac46444c5402250180bd03c82a5285b52d
Jonathan Fullam <jonathanfullam@gmail.com>
2013-01-01 17:07:32 -0500

    Validations!

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product attributes must not be empty " do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "price is great than 0" do
+    product = Product.new title: "unique", description:"blah, blah", image_url: "dkd.gif"
+    product.price = 0
+    assert product.invalid?
+    assert product.errors[:price].any?
+
+    product.price = 1
+    assert product.valid?
+    assert product.errors[:price].empty?
+  end
+
+  def create_new_product(image_url)
+    product = Product.new title: 'unique', description: "desc", price: 0.90, image_url: image_url
+  end
+
+  test "image url is valid" do
+    ok = ['one.gif', 'two.jpg', 'three.png']
+    bad = ['ald', 'ldk.xls', 'lskd.pdf']
+
+    ok.each  {|e| assert create_new_product(e).valid?, "#{e} should be valid"}
+    bad.each {|e| assert create_new_product(e).invalid?, "#{e} should NOT be valid"}
+
+  end
+
+  test 'title is unique' do
+
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy",
+                          price:       1,
+                          image_url:   "fred.gif")
+    p product
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
 end
